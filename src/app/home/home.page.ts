ce08d50b0de899c5c0edbc3fa22e997b3eab1665
Simon <simon.wicki@justwatch.com>
2018-08-15 23:33:10 +0200

    init ✨

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,10 +1,58 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { Observable, of, BehaviorSubject } from "rxjs";
+
+interface Message {
+  text: string;
+  userName: string;
+}
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  selector: "app-home",
+  templateUrl: "home.page.html",
+  styleUrls: ["home.page.scss"]
 })
 export class HomePage {
+	private initState: Message[] = [
+		{userName: 'init', text: 'random msg'},
+		{userName: 'init', text: 'random msg'},
+		{userName: 'init', text: 'random msg'},
+		{userName: 'init', text: 'random msg'},
+		{userName: 'init', text: 'random msg'},
+		{userName: 'init', text: 'random msg'},
+		{userName: 'init', text: 'random msg'},
+		{userName: 'init', text: 'random msg'},
+		{userName: 'init', text: 'random msg'},
+		{userName: 'init', text: 'random msg'},
+		{userName: 'init', text: 'random msg'},
+		{userName: 'init', text: 'random msg'},
+		{userName: 'init', text: 'random msg'},
+		{userName: 'init', text: 'random msg'},
+		{userName: 'init', text: 'random msg'},
+		{userName: 'init', text: 'random msg'},
+		{userName: 'init', text: 'random msg'},
+		{userName: 'init', text: 'random msg'},
+		{userName: 'init', text: 'random msg'},
+		{userName: 'init', text: 'random msg'},
+		{userName: 'init', text: 'random msg'},
+		{userName: 'init', text: 'random msg'},
+		{userName: 'init', text: 'random msg'},
+		{userName: 'init', text: 'random msg'},
+	];
+  private state$ = new BehaviorSubject(this.initState);
+  messages$: Observable<Message[]> = this.state$.asObservable();
 
+  constructor() {
+    of(1000, 2000, 3000, 4000, 5000).subscribe(delay => {
+	// delayed add of new messages
+      setTimeout(() => {
+        const values = this.state$.getValue();
+        const newState = [{
+			text: `message delayed: ${delay}`,
+			userName: "random"
+		  }].concat(values);
+		this.state$.next(newState);
+		console.log('added new value. total messages: ', values.length);
+      }, delay);
+    });
+  }
 }
