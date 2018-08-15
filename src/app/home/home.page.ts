0c022d83b4d06a00277c310efe47503ad42b5667
Simon <simon.wicki@justwatch.com>
2018-08-15 23:58:14 +0200

    added commented-out array variant

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -12,46 +12,54 @@ interface Message {
   styleUrls: ["home.page.scss"]
 })
 export class HomePage {
-	private initState: Message[] = [
-		{userName: 'init', text: 'random msg'},
-		{userName: 'init', text: 'random msg'},
-		{userName: 'init', text: 'random msg'},
-		{userName: 'init', text: 'random msg'},
-		{userName: 'init', text: 'random msg'},
-		{userName: 'init', text: 'random msg'},
-		{userName: 'init', text: 'random msg'},
-		{userName: 'init', text: 'random msg'},
-		{userName: 'init', text: 'random msg'},
-		{userName: 'init', text: 'random msg'},
-		{userName: 'init', text: 'random msg'},
-		{userName: 'init', text: 'random msg'},
-		{userName: 'init', text: 'random msg'},
-		{userName: 'init', text: 'random msg'},
-		{userName: 'init', text: 'random msg'},
-		{userName: 'init', text: 'random msg'},
-		{userName: 'init', text: 'random msg'},
-		{userName: 'init', text: 'random msg'},
-		{userName: 'init', text: 'random msg'},
-		{userName: 'init', text: 'random msg'},
-		{userName: 'init', text: 'random msg'},
-		{userName: 'init', text: 'random msg'},
-		{userName: 'init', text: 'random msg'},
-		{userName: 'init', text: 'random msg'},
-	];
+  private initState: Message[] = [
+    { userName: "init", text: "random msg" },
+    { userName: "init", text: "random msg" },
+    { userName: "init", text: "random msg" },
+    { userName: "init", text: "random msg" },
+    { userName: "init", text: "random msg" },
+    { userName: "init", text: "random msg" },
+    { userName: "init", text: "random msg" },
+    { userName: "init", text: "random msg" },
+    { userName: "init", text: "random msg" },
+    { userName: "init", text: "random msg" },
+    { userName: "init", text: "random msg" },
+    { userName: "init", text: "random msg" },
+    { userName: "init", text: "random msg" },
+    { userName: "init", text: "random msg" },
+    { userName: "init", text: "random msg" },
+    { userName: "init", text: "random msg" },
+    { userName: "init", text: "random msg" },
+    { userName: "init", text: "random msg" },
+    { userName: "init", text: "random msg" },
+    { userName: "init", text: "random msg" },
+    { userName: "init", text: "random msg" },
+    { userName: "init", text: "random msg" },
+    { userName: "init", text: "random msg" },
+    { userName: "init", text: "random msg" }
+  ];
+  // messages: Message[] = this.initState.concat();
+
   private state$ = new BehaviorSubject(this.initState);
   messages$: Observable<Message[]> = this.state$.asObservable();
 
   constructor() {
     of(1000, 2000, 3000, 4000, 5000).subscribe(delay => {
-	// delayed add of new messages
+      // delayed add of new messages
       setTimeout(() => {
         const values = this.state$.getValue();
-        const newState = [{
-			text: `message delayed: ${delay}`,
-			userName: "random"
-		  }].concat(values);
-		this.state$.next(newState);
-		console.log('added new value. total messages: ', values.length);
+        const newState = [
+          {
+            text: `message delayed: ${delay}`,
+            userName: "random"
+          }
+        ].concat(values);
+        this.state$.next(newState);
+
+        console.log("added new value. total messages: ", values.length);
+
+        // // also works with simple arrays
+        // this.messages = [{text: 'lulzy', userName: 'something'}].concat(this.messages);
       }, delay);
     });
   }
